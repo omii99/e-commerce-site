df44c05b8c424996afe148362cf50055c46bdf66
omii99 <mandarasharannya990116@gmail.com>
2023-01-17 20:34:43 +0530

    initial commit

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>Categories</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Accessories</span>
+          <span>New Arrivals</span>
+        </div>
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className="item">
+          <h1>About</h1>
+          <span>
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable
+          </span>
+        </div>
+        <div className="item">
+          <h1>About</h1>
+          <span>
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
